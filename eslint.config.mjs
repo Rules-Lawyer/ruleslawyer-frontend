3526b8bfa6ee4d81679cc14c1c6dca8e4e890b10
Mattie Schraeder <mattie@mattie.lgbt>
2026-06-04 21:32:27 -0500

    fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,10 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
 export default [
   {
@@ -19,5 +16,6 @@ export default [
       "next-env.d.ts",
     ],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
 ];
