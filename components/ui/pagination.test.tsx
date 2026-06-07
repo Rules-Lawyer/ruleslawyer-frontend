090302f0fa1fe5da28fb6b839f9a1cde158a6373
Mattie Schraeder <mattie@mattie.lgbt>
2026-06-07 01:01:13 -0500

    move these under ui

--- components/ui/pagination.test.tsx
+++ components/ui/pagination.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Pagination from "@/components/pagination";
+import Pagination from "@/components/ui/pagination";
 
 describe("Pagination", () => {
   it("renders nothing when there is only one page", () => {
